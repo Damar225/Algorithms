310391fcaf2793ae6272b776ae052b32f5ef9f8b
Damar225 <32112262+Damar225@users.noreply.github.com>
2021-12-16 18:33:06 +0200

    Update selectionSort.c

--- selectionSort.c
+++ selectionSort.c
@@ -2,48 +2,32 @@
 
 void main()
 {
-    int array[] = {2, 6, 7, 4, 11, 3, 2, 5, 1};
+    int array[] = {2, 6, 7, 4, 3, 2, 5, 1};
     int size = sizeof(array)/sizeof(array[0]);
 
     SelectionSort(array, size);
     PrintArray(array, size);
 }
 
-void SelectionSort(int unSortedArray[], int size)
+void SelectionSort(int array[], int size)
 {
-    int sortedArray[size];
-    for(int i = 0;i< size; i++)
+    for(int j = 0;j<size;j++)
     {
-        sortedArray[i] = LargestElement(unSortedArray, size-i);
-    }
-}
-
-int LargestElement(int array[], int size)
-{
-    int max = array[0];
-    int maxIndex = 0;
-    for(int i = 1;i< size; i++)
-    {
-        if(array[i] > max)
+        int min = array[j];
+        int minIndex = j;
+        for(int i = j; i < size; i++)
         {
-            max = array[i];
-            maxIndex = i;
+            if(array[i] < min)
+            {
+                min = array[i];
+                minIndex = i;
+            }
         }
+        int temp = array[j];
+        array[j] = min;
+        array[minIndex] = temp;
     }
-
-    // remove element from the array
-    int temp = array[maxIndex];
-    array[maxIndex] = array[size-1];
-    array[size-1] = temp;
-
-    return max;
 }
-
-void DeleteElementAtPosition(int array[], int size, int position)
-{
-
-}
-
 
 void PrintArray(int array[], int size)
 {
